26edbf6df2b0ae398a0abddaa86e542c4b887a36
Johannes Juks <johannesjuks@gmail.com>
2024-01-19 21:51:42 +0100

    Improve keymap format and add descriptions

--- lua/joei/maps.lua
+++ lua/joei/maps.lua
@@ -3,7 +3,8 @@ local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = " "
 
-km.set("n", "-", vim.cmd.Ex, { desc = "Edit current directory" })
+km.set("n", "-", ":Oil<cr>", { desc = "Open parent directory" })
+km.set("n", "<leader><leader>", "<c-6>", { desc = "Switch to most recent buffer" })
 
 -- km.set("n", "<leader>q", "<cmd>x<cr>", { desc = ""})
 km.set("n", "<leader>x", "<cmd>x<cr>", { desc = "Save -> Quit" })
@@ -15,3 +16,5 @@ km.set("n", "<leader>/", "<cmd>noh<cr>", { desc = "Turn off highlighting" })
 km.set("n", "<leader>rc", "<cmd>vsp $MYVIMRC<cr><cmd>cd %:p:h<cr>", { desc = "Vsplit vimrc" })
 km.set("n", "<leader>rh", "<cmd>edit $MYVIMRC<cr><cmd>cd %:p:h<cr>", { desc = "Edit vimrc" })
 km.set("n", "<leader>rr", "<cmd>so $MYVIMRC<cr>", { desc = "Reload vimrc" })
+
+km.set("n", "<leader>bo", "<cmd>%bd|e#|bd#<cr>", { desc = "Delete ALL buffers except current one" })
