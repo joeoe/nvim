fb5c3739690b78fbbdcb61e6aeaf835d65585187
Johannes Juks <johannesjuks@gmail.com>
2024-01-29 16:41:58 +0100

    Remove ts-hypr

--- lua/joei/plugins/treesitter.lua
+++ lua/joei/plugins/treesitter.lua
@@ -3,7 +3,6 @@ return {
 	build = ":TSUpdate",
 	config = function()
 		local configs = require("nvim-treesitter.configs")
-
 		configs.setup({
 			ensure_installed = {
 				"bash",
@@ -80,5 +79,15 @@ return {
 			},
 			indent = { enable = true },
 		})
+
+		-- local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+		-- parser_config.hypr = {
+		-- 	install_info = {
+		-- 		url = "https://github.com/luckasRanarison/tree-sitter-hypr",
+		-- 		files = { "src/parser.c" },
+		-- 		branch = "master",
+		-- 	},
+		-- 	filetype = "hypr",
+		-- }
 	end,
 }
