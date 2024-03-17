864b0997a6d55238a394aeabe2d61d64f2b14bf6
Johannes Juks <johannesjuks@gmail.com>
2024-03-17 20:27:47 +0100

    feat: replace copilot.lua with tpope's plugin

--- lua/joei/plugins/copilot.lua
+++ lua/joei/plugins/copilot.lua
@@ -1,65 +1,65 @@
 return {
-  enabled = false,
-  'zbirenbaum/copilot.lua',
-  cmd = 'Copilot',
-  event = 'InsertEnter',
-  config = function()
-    require('copilot').setup {
-      panel = {
-        enabled = true,
-        auto_refresh = false,
-        keymap = {
-          jump_prev = '[[',
-          jump_next = ']]',
-          accept = '<CR>',
-          refresh = 'gr',
-          open = '<C-CR>',
-        },
-        layout = {
-          position = 'bottom', -- | top | left | right
-
-          ratio = 0.4,
-        },
-      },
-      suggestion = {
-        enabled = true,
-        auto_trigger = false,
-        debounce = 75,
-        keymap = {
-          accept = '<M-l>',
-          accept_word = false,
-          accept_line = false,
-          next = '<M-]>',
-          prev = '<M-[>',
-          dismiss = '<C-]>',
-        },
-      },
-      filetypes = {
-        yaml = false,
-        markdown = false,
-        help = false,
-        gitcommit = false,
-        gitrebase = false,
-        hgcommit = false,
-        svn = false,
-        cvs = false,
-        ['.'] = false,
-      },
-      copilot_node_command = 'node', -- Node.js version must be > 18.x
-      server_opts_overrides = {},
-    }
-
-    -- hide copilot suggestions when cmp menu is open
-    -- to prevent odd behavior/garbled up suggestions
-    local cmp_status_ok, cmp = pcall(require, 'cmp')
-    if cmp_status_ok then
-      cmp.event:on('menu_opened', function()
-        vim.b.copilot_suggestion_hidden = true
-      end)
-
-      cmp.event:on('menu_closed', function()
-        vim.b.copilot_suggestion_hidden = false
-      end)
-    end
-  end,
+  'github/copilot.vim',
+  -- 'zbirenbaum/copilot.lua',
+  -- cmd = 'Copilot',
+  -- event = 'InsertEnter',
+  -- config = function()
+  --   require('copilot').setup {
+  --     panel = {
+  --       enabled = true,
+  --       auto_refresh = false,
+  --       keymap = {
+  --         jump_prev = '[[',
+  --         jump_next = ']]',
+  --         accept = '<CR>',
+  --         refresh = 'gr',
+  --         open = '<C-CR>',
+  --       },
+  --       layout = {
+  --         position = 'bottom', -- | top | left | right
+  --
+  --         ratio = 0.4,
+  --       },
+  --     },
+  --     suggestion = {
+  --       enabled = true,
+  --       auto_trigger = false,
+  --       debounce = 75,
+  --       keymap = {
+  --         accept = '<M-l>',
+  --         accept_word = false,
+  --         accept_line = false,
+  --         next = '<M-]>',
+  --         prev = '<M-[>',
+  --         dismiss = '<C-]>',
+  --       },
+  --     },
+  --     filetypes = {
+  --       yaml = false,
+  --       markdown = false,
+  --       help = false,
+  --       gitcommit = false,
+  --       gitrebase = false,
+  --       hgcommit = false,
+  --       svn = false,
+  --       cvs = false,
+  --       ['.'] = false,
+  --     },
+  --     copilot_node_command = 'node', -- Node.js version must be > 18.x
+  --     server_opts_overrides = {},
+  --   }
+  --
+  --   -- hide copilot suggestions when cmp menu is open
+  --   -- to prevent odd behavior/garbled up suggestions
+  --   local cmp_status_ok, cmp = pcall(require, 'cmp')
+  --   if cmp_status_ok then
+  --     cmp.event:on('menu_opened', function()
+  --       vim.b.copilot_suggestion_hidden = true
+  --     end)
+  --
+  --     cmp.event:on('menu_closed', function()
+  --       vim.b.copilot_suggestion_hidden = false
+  --     end)
+  --   end
+  -- end,
 }
