1c5094560b9b8852c6440f824d5f2aecf1aae13d
Johannes Juks <johannesjuks@gmail.com>
2024-03-17 20:29:57 +0100

    feat: add some ts & vue snippets

--- LuaSnip/typescript.lua
+++ LuaSnip/typescript.lua
@@ -1,10 +1,14 @@
 return {
-	s("im", fmta("import { <> } from '<>'", { i(2), i(1) })),
-	s("imp", fmta("import '<>'", { i(1) })),
+  s('im', fmta("import { <> } from '<>'", { i(2), i(1) })),
+  s('imp', fmta("import '<>'", { i(1) })),
 
-	s("cl", fmta("console.log(<>)", { i(1) })),
-	s("ce", fmta("console.error(<>)", { i(1) })),
+  s('cl', fmta('console.log(<>)', { i(1) })),
+  s('ce', fmta('console.error(<>)', { i(1) })),
 
-	s("uin", t("const { t } = useI18n()")),
-	s("tr", fmta('{{ t("<>") }}', { i(1) })),
+  s('uin', t 'const { t } = useI18n()'),
+  s('tr', fmta('{{ t("<>") }}', { i(1) })),
+
+  -- TODO: Separate test type
+  s('des', fmta("describe('<>', () =>> {\n\t<>\n})", { i(1), i(2) })),
+  s('it', fmta("test('<>', () =>> {\n\t<>\n})", { i(1), i(2) })),
 }
--- LuaSnip/vue.lua
+++ LuaSnip/vue.lua
@@ -1,3 +1,16 @@
 return {
-	s("cmp", fmt([[const {} = computed(() => {} )]], { i(1), i(2) })),
+  s('cmp', fmt([[const {} = computed(() => {} )]], { i(1), i(2) })),
+
+  s(
+    'vc',
+    fmt(
+      [[<script lang="ts" setup>
+  {}
+</script>
+
+<template>
+</template>]],
+      { i(1) }
+    )
+  ),
 }
