e798c50231b73c50aaaf72a829b8efeccf98aaa3
Johannes Juks <johannesjuks@gmail.com>
2024-03-17 20:29:31 +0100

    feat: move luasnip conf to cmp.lua

--- lua/joei/plugins/cmp.lua
+++ lua/joei/plugins/cmp.lua
@@ -12,12 +12,18 @@ return {
     'hrsh7th/cmp-nvim-lsp-signature-help',
     'petertriho/cmp-git',
     'onsails/lspkind-nvim',
-    'L3MON4D3/LuaSnip',
+    {
+      'L3MON4D3/LuaSnip',
+      build = 'make install_jsregexp',
+    },
     'saadparwaiz1/cmp_luasnip',
   },
   config = function()
     local cmp = require 'cmp'
     local luasnip = require 'luasnip'
+    local p = vim.fn.stdpath 'config' .. '/LuaSnip/'
+    luasnip.filetype_extend('vue', { 'typescript' })
+    require('luasnip.loaders.from_lua').lazy_load { paths = p }
 
     local has_words_before = function()
       unpack = unpack or table.unpack
@@ -25,56 +31,40 @@ return {
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match '%s' == nil
     end
 
-    cmp.setup {
-      mapping = {
-        ['<Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-          -- that way you will only jump inside the snippet region
-          -- elseif luasnip.expand_or_jumpable() then
-          -- 	luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
+    vim.keymap.set('n', '<leader>sr', function()
+      require('luasnip.loaders.from_lua').load { paths = p }
+      print 'Reload snippets'
+    end, { desc = 'Reload snippets' })
+    vim.keymap.set('n', '<leader>se', function()
+      require('luasnip.loaders').edit_snippet_files()
+    end, { desc = 'Edit snippets' })
 
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          -- elseif luasnip.jumpable(-1) then
-          -- 	luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { 'i', 's' }),
-        -- cmp.mapping.preset.insert({
+    -- vim.keymap.set({ 'i' }, '<c-l>', function()
+    --   if ls.choice_active() then
+    --     ls.change_choice(1)
+    --   end
+    -- end, { silent = true })
+
+    cmp.setup {
+      mapping = cmp.mapping.preset.insert {
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
-        ['<CR>'] = cmp.mapping.confirm { select = true }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-        ['<C-p>'] = cmp.mapping {
-          i = function()
-            if cmp.visible() then
-              cmp.select_prev_item()
-            else
-              cmp.complete()
-            end
-          end,
-        },
-        ['<C-n>'] = cmp.mapping {
-          i = function()
-            if cmp.visible() then
-              cmp.select_next_item()
-            else
-              cmp.complete()
-            end
-          end,
-        },
-        -- }),
+        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<CR>'] = cmp.mapping.confirm { select = true },
+        ['<C-l>'] = cmp.mapping(function()
+          if luasnip.expand_or_locally_jumpable() then
+            luasnip.expand_or_jump()
+          end
+        end, { 'i', 's' }),
+        ['<C-h>'] = cmp.mapping(function()
+          if luasnip.locally_jumpable(-1) then
+            luasnip.jump(-1)
+          end
+        end, { 'i', 's' }),
       },
       sources = cmp.config.sources {
         { name = 'nvim_lsp' },
@@ -83,6 +73,7 @@ return {
         { name = 'path' },
         { name = 'nvim_lsp_signature_help' },
       },
+      completion = { completeopt = 'menu,menuone,noinsert' },
       snippet = {
         expand = function(args)
           luasnip.lsp_expand(args.body)
