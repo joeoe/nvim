71fce79733b71ba26def74a241e45feda2340c29
Johannes Juks <johannesjuks@gmail.com>
2024-03-17 20:28:48 +0100

    feat: update lsp-related keybinds

--- lua/joei/plugins/lsp.lua
+++ lua/joei/plugins/lsp.lua
@@ -18,25 +18,51 @@ return {
         -- Enable completion triggered by <c-x><c-o>
         vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
 
+        local tsc = require 'telescope.builtin'
+
+        local map = function(keys, func, desc)
+          vim.keymap.set('n', keys, func, { buffer = ev.buf, desc = 'LSP: ' .. desc })
+        end
+
         -- Buffer local mappings.
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { buffer = ev.buf, desc = 'Go to declaration' })
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = ev.buf, desc = 'Go to definition' })
-        vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, { buffer = ev.buf, desc = 'Go to type definition' })
-        vim.keymap.set('n', 'gh', vim.lsp.buf.hover, { buffer = ev.buf, desc = 'Hover' })
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, { buffer = ev.buf, desc = 'Go to implementation' })
-        vim.keymap.set('n', '<M-k>', vim.lsp.buf.signature_help, { buffer = ev.buf, desc = 'Signature help' })
-        vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, { buffer = ev.buf, desc = 'Add workspace folder' })
-        vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, { buffer = ev.buf, desc = 'Remove workspace folder' })
-        vim.keymap.set('n', '<leader>wl', function()
+        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+        map('gd', tsc.lsp_definitions, '[G]oto [D]efinition')
+        map('gr', tsc.lsp_references, '[G]oto [R]eferences')
+        map('gi', tsc.lsp_implementations, '[G]oto [I]mplementations')
+        map('<leader>D', tsc.lsp_type_definitions, 'Type [D]efinition')
+        map('<leader>ds', tsc.lsp_document_symbols, '[D]ocument [S]ymbols')
+
+        map('<leader>rn', vim.lsp.buf.rename, 'Rename')
+        map('<leader>ca', vim.lsp.buf.code_action, 'Code action')
+        map('gh', vim.lsp.buf.hover, 'Hover documentation')
+        map('K', vim.lsp.buf.hover, 'Hover documentation')
+        map('<M-k>', vim.lsp.buf.signature_help, 'Signature help')
+        map('<leader>rs', ':LspRestart<cr>', 'Restart LSP')
+
+        map('<leader>ws', tsc.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map('<leader>wa', vim.lsp.buf.add_workspace_folder, 'Add workspace folder')
+        map('<leader>wr', vim.lsp.buf.remove_workspace_folder, 'Remove workspace folder')
+        map('<leader>wl', function()
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-        end, { buffer = ev.buf, desc = 'List workspace folder' })
-        vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = ev.buf, desc = 'Rename' })
-        vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, { buffer = ev.buf, desc = 'Code action' })
-        vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = ev.buf, desc = 'References' })
-        -- vim.keymap.set("n", "<leader>F", function()
-        -- 	vim.lsp.buf.format({ async = true })
-        -- end, { buffer = ev.buf, desc = "Format buffer" })
-        vim.keymap.set('n', '<leader>rs', ':LspRestart<cr>', { buffer = ev.buf, desc = 'Restart LSP' })
+        end, 'List workspace folder')
+
+        -- The following two autocommands are used to highlight references of the
+        -- word under your cursor when your cursor rests there for a little while.
+        --    See `:help CursorHold` for information about when this is executed
+        --
+        -- When you move your cursor, the highlights will be cleared (the second autocommand).
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client and client.server_capabilities.documentHighlightProvider then
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            buffer = ev.buf,
+            callback = vim.lsp.buf.document_highlight,
+          })
+
+          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            buffer = ev.buf,
+            callback = vim.lsp.buf.clear_references,
+          })
+        end
       end,
     })
   end,
