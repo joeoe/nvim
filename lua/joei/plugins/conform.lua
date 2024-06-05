6a5e917d12bbd51ee0f407be91ee0c3e4055c641
Johannes Juks <johannesjuks@gmail.com>
2024-06-05 20:38:40 +0200

    reformat some things

--- lua/joei/plugins/conform.lua
+++ lua/joei/plugins/conform.lua
@@ -23,23 +23,22 @@ return {
         vue = { { 'prettierd', 'prettier' } },
         nix = { { 'alejandra' } },
       },
-      format_on_save = function(bufnr)
-        -- Disable with a global or buffer-local variable
-        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
-          return
-        end
-        return { timeout_ms = 500, lsp_fallback = true }
-      end,
       -- Customize formatters
       formatters = {
         shfmt = {
           prepend_args = { '-i', '2' },
         },
       },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return {}
+        end
+        return { timeout_ms = 500, lsp_fallback = true }
+      end,
     }
   end,
   init = function()
-    -- If you want the formatexpr, here is the place to set it
     vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
   end,
 }
--- lua/joei/plugins/mason.lua
+++ lua/joei/plugins/mason.lua
@@ -1,203 +1,207 @@
 return {
-  enabled = true,
-  'williamboman/mason.nvim',
-  dependencies = {
-    'folke/neodev.nvim',
-    'folke/neoconf.nvim',
-    'williamboman/mason-lspconfig.nvim',
-    'WhoIsSethDaniel/mason-tool-installer.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
-  },
-  config = function()
-    require('neodev').setup {}
-    require('neoconf').setup {}
-    require('mason').setup {
-      ui = {
-        icons = {
-          package_installed = '✓',
-          package_pending = '➜',
-          package_uninstalled = '✗',
+  {
+    enabled = true,
+    'williamboman/mason.nvim',
+    dependencies = {
+      'folke/neoconf.nvim',
+      'williamboman/mason-lspconfig.nvim',
+      'WhoIsSethDaniel/mason-tool-installer.nvim',
+      'jay-babu/mason-nvim-dap.nvim',
+    },
+    config = function()
+      require('neoconf').setup {}
+      require('mason').setup {
+        ui = {
+          icons = {
+            package_installed = '✓',
+            package_pending = '➜',
+            package_uninstalled = '✗',
+          },
         },
-      },
-    }
+      }
 
-    local mason_lspconfig = require 'mason-lspconfig'
-    mason_lspconfig.setup {
-      ensure_installed = {
-        -- "tsserver",
-        'html',
-        'cssls',
-        -- "tailwindcss",
-        'lua_ls',
-        'rust_analyzer',
-        'gopls',
-      },
-      automatic_installation = true, -- not the same as ensure_installed
-    }
+      local mason_lspconfig = require 'mason-lspconfig'
+      mason_lspconfig.setup {
+        ensure_installed = {
+          -- "tsserver",
+          'html',
+          'cssls',
+          -- "tailwindcss",
+          'lua_ls',
+          'rust_analyzer',
+          'gopls',
+        },
+        automatic_installation = true, -- not the same as ensure_installed
+      }
 
-    require('mason-tool-installer').setup {
-      ensure_installed = {
-        'prettier', -- prettier formatter
-        'stylua', -- lua formatter
-        'eslint_d', -- js linter
-      },
-    }
+      require('mason-tool-installer').setup {
+        ensure_installed = {
+          'prettier', -- prettier formatter
+          'stylua', -- lua formatter
+          'eslint_d', -- js linter
+        },
+      }
 
-    require('mason-nvim-dap').setup {
-      ensure_installed = {},
-      handlers = {
-        function(config)
-          -- all sources with no handler get passed here
-          -- Keep original functionality
-          require('mason-nvim-dap').default_setup(config)
+      require('mason-nvim-dap').setup {
+        ensure_installed = {},
+        handlers = {
+          function(config)
+            -- all sources with no handler get passed here
+            -- Keep original functionality
+            require('mason-nvim-dap').default_setup(config)
+          end,
+        },
+        automatic_installation = true,
+      }
+
+      local lspconfig = require 'lspconfig'
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+      -- capabilities.textDocument.completion.completionItem.snippetSupport = true
+      capabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFoldingOnly = true }
+
+      local border = 'single'
+      -- single, double, rounded, solid, shadow
+
+      local handlers = {
+        -- ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
+        -- ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
+      }
+
+      mason_lspconfig.setup_handlers {
+        -- default handler
+        function(server_name)
+          lspconfig[server_name].setup {
+            capabilities = capabilities,
+            handlers = handlers,
+          }
         end,
-      },
-      automatic_installation = true,
-    }
 
-    local lspconfig = require 'lspconfig'
-
-    local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-    -- capabilities.textDocument.completion.completionItem.snippetSupport = true
-    capabilities.textDocument.foldingRange = { dynamicRegistration = false, lineFoldingOnly = true }
-
-    local border = 'single'
-    -- single, double, rounded, solid, shadow
-
-    local handlers = {
-      -- ['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = border }),
-      -- ['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = border }),
-    }
-
-    mason_lspconfig.setup_handlers {
-      -- default handler
-      function(server_name)
-        lspconfig[server_name].setup {
-          capabilities = capabilities,
-          handlers = handlers,
-        }
-      end,
-
-      ['lua_ls'] = function()
-        lspconfig.lua_ls.setup {
-          capabilities = capabilities,
-          handlers = handlers,
-          settings = {
-            Lua = {
-              runtime = { version = 'LuaJIT' },
-              workspace = {
-                checkThirdParty = false,
-                -- Tells lua_ls where to find all the Lua files that you have loaded
-                -- for your neovim configuration.
-                library = {
-                  '${3rd}/luv/library',
-                  unpack(vim.api.nvim_get_runtime_file('', true)),
+        ['lua_ls'] = function()
+          lspconfig.lua_ls.setup {
+            capabilities = capabilities,
+            handlers = handlers,
+            settings = {
+              Lua = {
+                runtime = { version = 'LuaJIT' },
+                workspace = {
+                  checkThirdParty = false,
+                  -- Tells lua_ls where to find all the Lua files that you have loaded
+                  -- for your neovim configuration.
+                  library = {
+                    '${3rd}/luv/library',
+                    unpack(vim.api.nvim_get_runtime_file('', true)),
+                  },
+                  -- If lua_ls is really slow on your computer, you can try this instead:
+                  -- library = { vim.env.VIMRUNTIME },
                 },
-                -- If lua_ls is really slow on your computer, you can try this instead:
-                -- library = { vim.env.VIMRUNTIME },
-              },
-              completion = {
-                callSnippet = 'Replace',
-              },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              diagnostics = { disable = { 'missing-fields' } },
-            },
-          },
-        }
-      end,
-
-      ['rust_analyzer'] = function()
-        lspconfig.rust_analyzer.setup {
-          capabilities = capabilities,
-          handlers = handlers,
-          cmd = {
-            'rustup',
-            'run',
-            'stable',
-            'rust-analyzer',
-          },
-          settings = {
-            ['rust-analyzer'] = {
-              assist = {
-                importMergeBehavior = 'last',
-                importPrefix = 'by_self',
-              },
-              diagnostics = {
-                disabled = { 'unresolved-import' },
-              },
-              cargo = {
-                loadOutDirsFromCheck = true,
-              },
-              procMacro = {
-                enable = true,
-              },
-              checkOnSave = {
-                command = 'clippy',
+                completion = {
+                  callSnippet = 'Replace',
+                },
+                -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+                diagnostics = { disable = { 'missing-fields' } },
               },
             },
-          },
-        }
-      end,
+          }
+        end,
 
-      ['tsserver'] = function()
-        lspconfig.tsserver.setup {
-          capabilities = capabilities,
-          handlers = handlers,
-          init_options = {
-            plugins = {
-              {
-                name = '@vue/typescript-plugin',
-                location = 'any/path',
-                languages = { 'javascript', 'typescript', 'vue' },
+        ['rust_analyzer'] = function()
+          lspconfig.rust_analyzer.setup {
+            capabilities = capabilities,
+            handlers = handlers,
+            cmd = {
+              'rustup',
+              'run',
+              'stable',
+              'rust-analyzer',
+            },
+            settings = {
+              ['rust-analyzer'] = {
+                assist = {
+                  importMergeBehavior = 'last',
+                  importPrefix = 'by_self',
+                },
+                diagnostics = {
+                  disabled = { 'unresolved-import' },
+                },
+                cargo = {
+                  loadOutDirsFromCheck = true,
+                },
+                procMacro = {
+                  enable = true,
+                },
+                checkOnSave = {
+                  command = 'clippy',
+                },
               },
             },
-            preferences = {
-              diagnosticOptions = {
-                enable = true,
-                exclude = { '**/node_modules/**' },
+          }
+        end,
+
+        ['tsserver'] = function()
+          lspconfig.tsserver.setup {
+            capabilities = capabilities,
+            handlers = handlers,
+            init_options = {
+              plugins = {
+                {
+                  name = '@vue/typescript-plugin',
+                  location = 'any/path',
+                  languages = { 'javascript', 'typescript', 'vue' },
+                },
+              },
+              preferences = {
+                diagnosticOptions = {
+                  enable = true,
+                  exclude = { '**/node_modules/**' },
+                },
               },
             },
-          },
-          filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx', 'vue' },
-          -- root_dir = require('lspconfig.util').root_pattern('.git', 'tsconfig.json', 'package.json', 'jsconfig.json'),
-        }
-      end,
+            filetypes = { 'javascript', 'javascriptreact', 'javascript.jsx', 'typescript', 'typescriptreact', 'typescript.tsx', 'vue' },
+            -- root_dir = require('lspconfig.util').root_pattern('.git', 'tsconfig.json', 'package.json', 'jsconfig.json'),
+          }
+        end,
 
-      ['volar'] = function()
-        lspconfig.volar.setup {
-          capabilities = capabilities,
-          handlers = handlers,
-          filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
-          -- root_dir = require('lspconfig.util').root_pattern('.git', 'package.json'),
-        }
-      end,
+        ['volar'] = function()
+          lspconfig.volar.setup {
+            capabilities = capabilities,
+            handlers = handlers,
+            filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue', 'json' },
+            -- root_dir = require('lspconfig.util').root_pattern('.git', 'package.json'),
+          }
+        end,
 
-      ['clangd'] = function()
-        lspconfig.clangd.setup {
-          capabilities = capabilities,
-          handlers = handlers,
-          cmd = {
-            'clangd',
-            -- "--log=verbose",
-          },
-        }
-      end,
+        ['clangd'] = function()
+          lspconfig.clangd.setup {
+            capabilities = capabilities,
+            handlers = handlers,
+            cmd = {
+              'clangd',
+              -- "--log=verbose",
+            },
+          }
+        end,
 
-      ['jsonls'] = function()
-        local cap = capabilities
-        cap.textDocument.completion.completionItem.snippetSupport = true
-        lspconfig.jsonls.setup {
-          capabilities = cap,
-          handlers = handlers,
-        }
-      end,
+        ['jsonls'] = function()
+          local cap = capabilities
+          cap.textDocument.completion.completionItem.snippetSupport = true
+          lspconfig.jsonls.setup {
+            capabilities = cap,
+            handlers = handlers,
+          }
+        end,
 
-      ['gopls'] = function()
-        lspconfig.gopls.setup {
-          capabilities = capabilities,
-          handlers = handlers,
-        }
-      end,
-    }
-  end,
+        ['gopls'] = function()
+          lspconfig.gopls.setup {
+            capabilities = capabilities,
+            handlers = handlers,
+          }
+        end,
+      }
+    end,
+  },
+  {
+    'folke/lazydev.nvim',
+    ft = 'lua',
+  },
 }
