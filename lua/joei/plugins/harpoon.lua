524ff37e401018f08c7fce9e694c2a1b0399a88f
Johannes Juks <johannesjuks@proton.me>
2024-06-16 23:39:17 +0200

    add number binds to harpoon

--- lua/joei/plugins/harpoon.lua
+++ lua/joei/plugins/harpoon.lua
@@ -4,7 +4,9 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
   config = function()
     local harpoon = require 'harpoon'
+    local extensions = require 'harpoon.extensions'
     harpoon:setup()
+    harpoon:extend(extensions.builtins.navigate_with_number())
 
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
@@ -12,6 +14,25 @@ return {
     -- vim.keymap.set('n', '<C-e>', function()
     --   harpoon.ui:toggle_quick_menu(harpoon:list())
     -- end)
+
+    vim.keymap.set('n', '<C-1>', function()
+      harpoon:list():select(1)
+    end)
+    vim.keymap.set('n', '<C-2>', function()
+      harpoon:list():select(2)
+    end)
+    vim.keymap.set('n', '<C-3>', function()
+      harpoon:list():select(3)
+    end)
+    vim.keymap.set('n', '<C-4>', function()
+      harpoon:list():select(4)
+    end)
+    vim.keymap.set('n', '<C-5>', function()
+      harpoon:list():select(5)
+    end)
+    vim.keymap.set('n', '<C-6>', function()
+      harpoon:list():select(6)
+    end)
 
     -- Toggle previous & next buffers stored within Harpoon list
     vim.keymap.set('n', '<C-S-P>', function()
